a8e75d807171a96bb35fb4c9f9470401bc43f228
jaycossey <jaycossey@live.com>
2024-03-14 17:45:36 +0000

    updated about section

--- src/components/about-components/About.jsx
+++ src/components/about-components/About.jsx
@@ -1,8 +1,8 @@
 import Card from '../containers/Card';
 
 const About = (props) => {
-    const aboutText = "When I began programming, I was awoken to a world from my childhood dreams. Bringing back a love for the endless possibilities, video game ideas and technology as a whole. I remembered a story that I could tell through a game, a story as old as time itself. Now, still very much learning every day, I have discovered skills and concepts that will help me truly realise that dream. I started with learning responsive web development. Gaining the foundations to the muscle memory of HTML, CSS and Javascript. Then I moved on to the concepts of computer science as a whole, gaining insight into algorithmic thinking, logical approaches to problems, and planning (which I cannot stress enough). I have experience working both on my own, and within a team, both inside and outside the world of code, though I prefer a collaborative approach. I offer a professional, organised, motivated service for front end development, especially for the weird and wonderful.";
-    
+    const aboutText = "As a software developer skilled in HTML, CSS, JavaScript, React, and Node.js, I specialize in delivering comprehensive solutions across various domains. With a focus on front-end web development, I design captivating interfaces and optimize website performance for seamless user experiences. Beyond web design, I leverage automation to streamline processes and enhance efficiency, utilizing Node.js to build robust backend systems that automate workflows, reducing manual intervention and errors. My expertise extends to crafting bespoke software solutions tailored to meet the unique needs of each client, enabling businesses to thrive in today's digital landscape.";
+
     return (
         <>
             <div id="aboutSection" 
@@ -17,7 +17,7 @@ const About = (props) => {
                             gap-10">
                 <Card text="About Me" />
                 
-                <Card text={aboutText} />
+                <Card text={`${aboutText}`} />
                 {/* EMPTY SPACE NEEDS FILLING */}
             </div>
         </>
